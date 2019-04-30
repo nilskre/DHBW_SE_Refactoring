ed8f27b8eb2c0573a65a07ca1fe6602ddad557f6
Nils Krehl <nils.krehl@lherk.de>
2019-04-30 16:20:23 +0200

    rename fields in Program

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -6,20 +6,24 @@ import java.util.*;
 class Customer {
     private String name;
     private Vector rentals = new Vector();
-    public Customer (String newname){
+
+    public Customer(String newname) {
         name = newname;
-    };
+    }
+
     public void addRental(Rental arg) {
         rentals.addElement(arg);
-    };
-    public String getName (){
+    }
+
+    public String getName() {
         return name;
-    };
+    }
+
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();
-        String result = "main.java.Rental Record for " + this.getName() + "\n";
+        String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
@@ -28,12 +32,12 @@ class Customer {
             //determine amounts for each line
             thisAmount = amountFor(each);
             // add frequent renter points
-            frequentRenterPoints ++;
+            frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints ++;
+                frequentRenterPoints++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
         //add footer lines
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -7,25 +7,24 @@ import java.lang.*;
  * by running "java -jar JarFile.jar" in the command line.
  * Java console applications can not be previewed in the Compilr IDE, only applets can.
  */
-public class Program
-{
+public class Program {
     /**
      * This is the main entry point for the application
      */
 
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         String result;
-        System.out.println("Welcome to the main.java.Movie Store");
-        Movie m1 = new Movie("movie1", 1);
-        Movie m2 = new Movie("movie2", 2);
-        Rental r1 = new Rental(m1, 10);
-        Rental r2 = new Rental(m2, 5);
-        Customer c1 = new Customer("joe");
-        c1.addRental(r1);   c1.addRental(r2);
+        System.out.println("Welcome to the Movie Store");
+        Movie movie1 = new Movie("movie1", 1);
+        Movie movie2 = new Movie("movie2", 2);
+        Rental rental1 = new Rental(movie1, 10);
+        Rental rental2 = new Rental(movie2, 5);
+        Customer customer1 = new Customer("joe");
+        customer1.addRental(rental1);
+        customer1.addRental(rental2);
         System.out.println("Let's get the Statement");
-        result = c1.statement();
+        result = customer1.statement();
         System.out.println(result);
     }
 }
