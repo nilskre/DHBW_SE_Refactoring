a6f9cf79e56148163f9ad76b013c667c926ccfeb
Nils Krehl <nils.krehl@lherk.de>
2019-04-30 16:59:36 +0200

    rename fields

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -37,15 +37,16 @@ class Customer {
         String allRentalDates = "";
         while (enum_rentals.hasMoreElements()) {
             double thisAmount = 0;
-            Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            thisAmount = amountFor(each);
+            Rental rental = (Rental) enum_rentals.nextElement();
+            //determine amounts for rental line
+            thisAmount = getAmountFor(rental);
             frequentRenterPoints = addFrequentRenterPoints(frequentRenterPoints);
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            boolean movieIsNewRelease = rental.getMovie().getPriceCode() == Movie.NEW_RELEASE;
+            if (movieIsNewRelease && rental.getDaysRented() > 1)
                 frequentRenterPoints++;
             //show figures for this rental
-            allRentalDates += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            allRentalDates += "\t" + rental.getMovie().getTitle() + "\t" + "\t" + rental.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
 
@@ -60,7 +61,7 @@ class Customer {
         return frequentRenterPoints;
     }
 
-    private double amountFor(Rental each) {
+    private double getAmountFor(Rental each) {
         double thisAmount = 0;
         switch (each.getMovie().getPriceCode()) {
             case Movie.REGULAR:
